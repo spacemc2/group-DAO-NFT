894cac801553bc432100197d857fdbcaa56e36ca
Miguel Torres <mtorresroadby@gmail.com>
2023-07-05 22:07:04 -0400

    modifying frontend app

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,31 @@
-import '@/styles/globals.css'
+import "@rainbow-me/rainbowkit/styles.css";
+import "@/styles/globals.css";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { sepolia } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, publicClient } = configureChains([sepolia], [publicProvider()]);
+
+const { connectors } = getDefaultWallets({
+  appName: "CryptoDevs DAO",
+  projectId: "677531ff33e4836eac7952f6b9d27921",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
